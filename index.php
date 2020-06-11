c60d124958619bf2fd126480b482d8900465c861
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-11 17:27:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ var map = new kakao.maps.Map(mapContainer, mapOption),
     infowindow = new kakao.maps.InfoWindow({removable: true});
 
 // 지도에 영역데이터를 폴리곤으로 표시합니다 
-$.getJSON("https://github.com/jaksal829/gunan/blob/master/1234.geojson", function(geojson) {
+$.getJSON("1234.geojson", function(geojson) {
  
     var data = geojson.features;
     var coordinates = [];    //좌표 저장할 배열
@@ -35,7 +35,7 @@ $.getJSON("https://github.com/jaksal829/gunan/blob/master/1234.geojson", functio
         coordinates = val.geometry.coordinates;
         name = val.properties.SIG_KOR_NM;
         
-        displayArea(coordinates, name);
+        displayArea(coordinates);
  
     })
 })
@@ -44,16 +44,13 @@ $.getJSON("https://github.com/jaksal829/gunan/blob/master/1234.geojson", functio
 var polygons=[];                //function 안 쪽에 지역변수로 넣으니깐 폴리곤 하나 생성할 때마다 배열이 비어서 클릭했을 때 전체를 못 없애줌.  그래서 전역변수로 만듦.
     
 //행정구역 폴리곤
-function displayArea(coordinates, name) {
+function displayArea(coordinates) {
  
     var path = [];            //폴리곤 그려줄 path
     var points = [];        //중심좌표 구하기 위한 지역구 좌표들
     
     $.each(coordinates[0], function(index, coordinate) {        //console.log(coordinates)를 확인해보면 보면 [0]번째에 배열이 주로 저장이 됨.  그래서 [0]번째 배열에서 꺼내줌.
-        var point = new Object(); 
-        point.x = coordinate[1];
-        point.y = coordinate[0];
-        points.push(point);
+        
         path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));            //new kakao.maps.LatLng가 없으면 인식을 못해서 path 배열에 추가
     })
     
@@ -105,9 +102,9 @@ function displayArea(coordinates, name) {
         var level = map.getLevel()-2;
         
         // 지도를 클릭된 폴리곤의 중앙 위치를 기준으로 확대합니다
-        map.setLevel(level, {anchor: centroid(points), animate: {
-            duration: 350            //확대 애니메이션 시간
-        }});            
+        // map.setLevel(level, {anchor: centroid(points), animate: {
+        //     duration: 350            //확대 애니메이션 시간
+        // }});            
  
         deletePolygon(polygons);                    //폴리곤 제거      
     });
