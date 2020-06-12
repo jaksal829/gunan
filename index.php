f8cb025e7f6d150de05f70ef89aac08dd4a70c6f
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-12 16:57:02 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7982,7 +7982,7 @@ var map = new kakao.maps.Map(mapContainer, mapOption),
 for (var a = 0; a < areas.length; a++) {
     displayArea(areas[a]);
 }
-var polygons=[]; 
+var po=[]; 
 // 다각형을 생상하고 이벤트를 등록하는 함수입니다
 function displayArea(area) {
 
@@ -7996,7 +7996,7 @@ function displayArea(area) {
         fillColor: '#fff',
         fillOpacity: 0.7 
     });
-    polygons.push(polygon); 
+    po.push(polygon); 
     // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경합니다 
     // 지역명을 표시하는 커스텀오버레이를 지도위에 표시합니다
     kakao.maps.event.addListener(polygon, 'mouseover', function(mouseEvent) {
@@ -8020,7 +8020,28 @@ function displayArea(area) {
         polygon.setOptions({fillColor: '#fff'});
         customOverlay.setMap(null);
     }); 
-
+    function deletePolygon(po) {
+      for (var i = 0; i < po.length; i++) {
+          po[i].setMap(null);
+      }
+      po = [];
+    }
+    function centroid (points) {
+      var i, j, len, p1, p2, f, area, x, y;
+  
+      area = x = y = 0;
+  
+      for (i = 0, len = points.length, j = len - 1; i < len; j = i++) {
+              p1 = points[i];
+              p2 = points[j];
+  
+              f = p1.y * p2.x - p2.y * p1.x;
+              x += (p1.x + p2.x) * f;
+              y += (p1.y + p2.y) * f;
+              area += f * 3;
+      }
+      return new daum.maps.LatLng(x / area, y / area);
+    }
     // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 다각형의 이름과 면적을 인포윈도우에 표시합니다 
     kakao.maps.event.addListener(polygon, 'click', function() {
       var level = map.getLevel()-2;
@@ -8067,30 +8088,9 @@ function displayArea(area) {
       map.setLevel(level, {anchor: centroid(points), animate: {
         duration: 350            //확대 애니메이션 시간
       }});  
-      //deletePolygon(polygons);
+      deletePolygon(po);
     });
-    function deletePolygon(polygons) {
-      for (var i = 0; i < polygons.length; i++) {
-          polygons[i].setMap(null);
-      }
-      polygons = [];
-    }
-    function centroid (points) {
-      var i, j, len, p1, p2, f, area, x, y;
-  
-      area = x = y = 0;
-  
-      for (i = 0, len = points.length, j = len - 1; i < len; j = i++) {
-              p1 = points[i];
-              p2 = points[j];
-  
-              f = p1.y * p2.x - p2.y * p1.x;
-              x += (p1.x + p2.x) * f;
-              y += (p1.y + p2.y) * f;
-              area += f * 3;
-      }
-      return new daum.maps.LatLng(x / area, y / area);
-    }
+    
 }
 
 //map.setDraggable(false);
