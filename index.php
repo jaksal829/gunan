2e7a38c6f65669fd8309173fc105f50ec6c468c2
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-15 17:26:47 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php
+$connectionInfo = array("UID" => "gunan", "pwd" => "app2020!", "Database" => "covid", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gunan.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+    
+    if (!$conn) {
+        echo "conn: false";
+    }
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $row = 1;
+    $handle = fopen("latlng.csv", "r+");
+    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+        $num = count($data);
+        //echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+
+        $sql = false;
+        $sql = "INSERT INTO latlng VALUES (";
+
+        for ($c=0; $c < $num; $c++) {
+            $sql .= "'" . $data[$c] . "'";
+            if($c+1 !== $num){
+                $sql .= ", ";
+            }
+        }
+        $sql .= ");";
+        $getResults=sqlsrv_query($conn,$sql);
+        echo "$sql<br />";
+    }
+    fclose($handle);
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);  
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -107,41 +143,6 @@
 </head>
 <body>
   <?php
-    $connectionInfo = array("UID" => "gunan", "pwd" => "app2020!", "Database" => "covid", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:gunan.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-    
-    if (!$conn) {
-        echo "conn: false";
-    }
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $row = 1;
-    $handle = fopen("latlng.csv", "r+");
-    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-        $num = count($data);
-        //echo "<p> $num fields in line $row: <br /></p>\n";
-        $row++;
-
-        $sql = false;
-        $sql = "INSERT INTO latlng VALUES (";
-
-        for ($c=0; $c < $num; $c++) {
-            $sql .= "'" . $data[$c] . "'";
-            if($c+1 !== $num){
-                $sql .= ", ";
-            }
-        }
-        $sql .= ");";
-        $getResults=sqlsrv_query($conn,$sql);
-        echo "$sql<br />";
-    }
-    fclose($handle);
-    sqlsrv_free_stmt($stmt);
-    sqlsrv_close($conn);  
-
     ini_set("allow_url_fopen",1);
     include "simple_html_dom.php";
     //include "korea.geojson";
