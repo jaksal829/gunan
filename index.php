2f9e6c51445d8bca47dfc16ea738f38c139a0a89
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-15 18:51:12 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -34,8 +34,11 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     $junggu = "SELECT lat, lng FROM busan WHERE name='junggu'";
     $getResults1 = sqlsrv_query($conn,$junggu);
     while ($row = sqlsrv_fetch_array($getResults1,SQLSRV_FETCH_ASSOC)){
-      echo $row('lat')."<br>".$row('lng');
+      echo $row('lat')
+      echo "<br>";
+      echo $row('lng');
     }
+    sqlsrv_free_stst($stst);
     sqlsrv_close($conn);
 ?>
 <!DOCTYPE html>
