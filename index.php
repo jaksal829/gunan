ea0d8eb68fa27c74bd511fb58376a511b7921bf1
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-17 19:27:55 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -168,6 +168,9 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     .info .title {
       font-weight: bold;
     }
+    .hAddr {position:absolute;left:10px;top:10px;border-radius: 2px;background:#fff;background:rgba(255,255,255,0.8);z-index:1;padding:5px;}
+    #centerAddr {display:block;margin-top:2px;font-weight: normal;}
+    .bAddr {padding:5px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;}
     </style>
     <title>COVID-19</title>
 </head>
