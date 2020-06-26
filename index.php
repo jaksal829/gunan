b4828bb56bcebfa9a8d7671aff9bc6f30f937037
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-26 17:17:09 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -8266,16 +8266,16 @@ function displayArea(area) {
         kakao.maps.event.addListener(marker, 'click', makerClick(map,marker,infowindow));
       }
       <?
-      for(var mk2 = 0; mk2 < 8;mk2 ++){
+      for($mk2 = 0; $mk2 < 8;$mk2 ++){
         ?>
         var marker2 = new kakao.maps.Marker({
           map: map,
-          position: new kakao.maps.LatLng(<? echo $lat[mk2]; ?>,<? echo $lng[mk2]; ?>),
+          position: new kakao.maps.LatLng(<? echo $lat[$mk2]; ?>,<? echo $lng[$mk2]; ?>),
           image: markerImage
         })
         markers2.push(marker2)
-        // if(<? echo $period[mk2]; ?> < 14){
-        //   var infowindow2 = new kakao.maps.InfoWindow({content : '<div style="padding:5px;">위치 : <br><p>기간 : <? echo $period[mk2]; ?></p></div>'});
+        // if(<? echo $period[$mk2]; ?> < 14){
+        //   var infowindow2 = new kakao.maps.InfoWindow({content : '<div style="padding:5px;">위치 : <br><p>기간 : <? echo $period[$mk2]; ?></p></div>'});
         //   kakao.maps.event.addListener(marker2, 'click', makerClick(map,marker2,infowindow2));
         // }
         
